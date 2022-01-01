6fd9ff84e1ec926c06de2e9a552607f4a1bd5ef8
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-01 16:10:29 -0500

    moyenne ponderee

---
+++ cap1/media_ponderada.js
@@ -0,0 +1,15 @@
+console.log("Entrar com 4 numeros e imprimir a média ponderada, os pesos sao 1, 2, 3, 4");
+console.log("--------------------------------------------------------------------------");
+console.log("\n");
+
+let a = 5;
+let b = 7;
+let c = 9;
+let d = 10;
+let peso = 1 + 2 + 3 + 4; 
+let mp = 0;
+
+mp = (a*1 + b*2 + c*3 + d*4)/peso;
+
+console.log("A média ponderada é: " + mp.toFixed(2));
+
