f93d13580f07072c328de39536b66c6352607c64
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-01 15:58:31 -0500

    Exercice du calcule du logarithme

---
+++ cap1/logatitmo_de_numero.js
@@ -0,0 +1,9 @@
+console.log("Entre com um numero e imprima o logaritmo desse numero na base escolhida");
+console.log("----------------------------------------------------------------");
+
+let base = 2;
+let logaritmo = 32;
+
+let resultat = Math.log(logaritmo) / Math.log(base);
+
+console.log("O logaritmo de " + logaritmo + " na base " + base + " = " + resultat);
