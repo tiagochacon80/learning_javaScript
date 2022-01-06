4f6d856eab2c081a52ab659b8c0905fa2dabee02
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-06 18:38:28 -0500

    programme que calcule le rayon d'un cercle

---
+++ cap1/calculer_le_rayon_un_cercle.js
@@ -0,0 +1,8 @@
+//le rayon d'un cercle et imprimez le résultat suivant : périmètre et aire
+
+let rayon = 3;
+let perimetre = 2 * 3.14 * rayon;
+let aire = 3.14 * rayon ** 2;
+
+console.log("Périmètre d'un cercle = " + perimetre);
+console.log("Aire d'un cercle = " + aire);
