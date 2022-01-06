29a4b9c7238a2ea1d86337254a819734e4d91b7b
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-05 20:50:26 -0500

    programme calculer la racine carree

---
+++ cap1/calculer_raiz_quadrada_de_um_numero.js
@@ -0,0 +1,10 @@
+console.log("Entrer un numero et informer son carré et la racine carrée");
+console.log("----------------------------------------------------------");
+let nombre = 9;
+let carre = nombre ** 2; //nombre * nombre
+
+let racineCarree = Math.sqrt(nombre); //function pour savoir la racine carrée
+
+console.log("Nombre " + nombre);
+console.log("Carré " + carre);
+console.log("Racine carrée " + racineCarree);
