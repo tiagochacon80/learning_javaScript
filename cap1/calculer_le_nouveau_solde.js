0e3430d0fde60efcc52fc658561fc52d69f01d92
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-05 20:52:06 -0500

    programme pour calculer un solde de reajustement

---
+++ cap1/calculer_le_nouveau_solde.js
@@ -0,0 +1,8 @@
+console.log("Calculer le nouveau solde compte tenu du réajustement 1%");
+console.log("--------------------------------------------------------");
+
+let montant = 95;
+let reajustement = 0.01;
+let nouveauSolde = montant * (reajustement + 1);
+
+console.log("Le nouveau solde est " + nouveauSolde);
