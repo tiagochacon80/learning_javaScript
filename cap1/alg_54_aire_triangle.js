5e2f2701b5ff1f8c22edb194fb13fc607ee4ad88
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-08 17:22:21 -0500

    programme que calcule aire d'un triangle

---
+++ cap1/alg_54_aire_triangle.js
@@ -0,0 +1,9 @@
+//Créez un algorithme qui calcule et imprime l'aire d'un triangle.
+
+
+let base = 4;
+let hauter = 3
+
+let aire = (base * hauter)/2;
+
+console.log("L'aire d'un trinagle est = " + aire);
