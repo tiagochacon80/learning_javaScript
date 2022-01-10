c3fc1af7625e3ea2c7e423c26235396ef3dc7eb5
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-09 21:41:31 -0500

    programme aire d'un losange

---
+++ cap1/alg_55_aire_un_losange.js
@@ -0,0 +1,7 @@
+//Créez un algorithme qui calcule et imprime la surface d'un loganse.
+
+let diagplusgrand = 8;
+let diagmoyengrand = 4;
+let aire = (diagplusgrand * diagmoyengrand)/2;
+
+console.log("L'aire d'un losange = " + aire);
