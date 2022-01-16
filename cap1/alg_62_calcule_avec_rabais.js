092f3c0f9b8b7fa9d980616a5eb966f449736ab7
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-16 14:16:08 -0500

    programme qui calculer les rabais

---
+++ cap1/alg_62_calcule_avec_rabais.js
@@ -0,0 +1,7 @@
+//saisir la valeur de un produit et imprimez la nouvelle valeur étant donné que la remise était de 9 %.
+
+let produit = 15;
+let rabais = produit * 0.09;
+let nouveauMontnat = produit - rabais;
+
+console.log(nouveauMontnat.toFixed(2));
