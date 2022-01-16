a32d66b65fd0c5a4b4603186285f43c8c4c8909e
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-16 14:14:33 -0500

    calculer une PA

---
+++ cap1/alg_60_calculer_une_PA.js
@@ -0,0 +1,9 @@
+//Entrez la raison d'une PA et la valeur du 1º terme. Calculer imprimer le 10ème terme de la série
+
+let dexieme = 0;
+let raizon = 2;
+let terme = 2;
+
+dexieme = terme + 9*raizon;
+
+console.log(dexieme);
