b9aa1d3c5002648aa0f289621780d9ec5502ea1d
Tiago Chacon <tiagochacon80@gmail.com>
2022-01-19 20:35:04 -0500

    calculer quantite litres essence une voyage

---
+++ cap1/alg_66_calculer_quantite_litres_essence_une_voyage.js
@@ -0,0 +1,10 @@
+//Calculer la quantité de litres de carburant dépensés en voyage,
+//sachant que la voiture parcourt 12 km avec un litre. Le temps dépensé doit être fourni et la vitesse moyenne.
+
+
+let temps = 4;
+let vitesse = 120;
+let distance = temps * vitesse;
+let litres = distance/12;
+
+console.log(" Temps = " + temps + "h " + " vitesse = " + vitesse + "km/h " + " distance = " + distance + " litres = " + litres);
